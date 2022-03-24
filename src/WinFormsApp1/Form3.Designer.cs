98a18fd3c50fc5cf3b6b53d6aeef52d26333527a
buditato <datajanoe@gmail.com>
2022-03-25 02:01:53 +0700

    terakhirUI

--- src/WinFormsApp1/Form3.Designer.cs
+++ src/WinFormsApp1/Form3.Designer.cs
@@ -39,19 +39,19 @@
             // 
             this.pictureBox1.BackgroundImage = global::WinFormsApp1.Properties.Resources.ErrorbelomMilihBDFSBFS;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -5);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(552, 235);
+            this.pictureBox1.Size = new System.Drawing.Size(368, 157);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // closeButton
             // 
             this.closeButton.Image = global::WinFormsApp1.Properties.Resources.Button;
-            this.closeButton.Location = new System.Drawing.Point(500, 6);
-            this.closeButton.Margin = new System.Windows.Forms.Padding(4);
+            this.closeButton.Location = new System.Drawing.Point(333, 4);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(41, 41);
+            this.closeButton.Size = new System.Drawing.Size(27, 27);
             this.closeButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.closeButton.TabIndex = 3;
             this.closeButton.TabStop = false;
@@ -60,15 +60,15 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(233)))), ((int)(((byte)(216)))));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(241)))));
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button1.FlatAppearance.BorderSize = 3;
             this.button1.Font = new System.Drawing.Font("Tahoma", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(363, 147);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(242, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 52);
+            this.button1.Size = new System.Drawing.Size(86, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "sip";
             this.button1.UseVisualStyleBackColor = false;
@@ -77,14 +77,15 @@
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 225);
+            this.ClientSize = new System.Drawing.Size(367, 150);
             this.ControlBox = false;
             this.Controls.Add(this.button1);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form3";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
