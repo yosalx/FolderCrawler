52d33a91edb56bc285cebf064683b50b5ca3b6c6
buditato <datajanoe@gmail.com>
2022-03-25 21:41:16 +0700

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Tubes-2-Stima
+# TUGAS BESAR 2 -  STRATEGI ALGORITMA : Pengaplikasian Algoritma BFS dan DFS dalam Implementasi Folder Crawling 
+> Program sederhana yang dibuat oleh Kelompok Bing Slamet
+
+
+## Table of Contents
+* [Introduction](#introduction)
+* [General Info](#general-information)
+* [Requirements](#requirements)
+* [How to Run](#how-to-run)
+* [Project Status](#project-status)
+* [Sources](#sources)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+
+  
+## Introduction
+Hello, everyone! Welcome to our Github Repository!
+This project was created by the team you can see on the following table.
+| No. | Name | Student ID |
+| :---: | :---: | :---: |
+| 1. | Arik Rayi Arkananta | 13520048 |
+| 2. | Januar Budi Ghifari | 13520132 | 
+| 3. | Yoseph Alexander Siregar | 13520141 |
+
+## General Information
+Program ini dibuat untuk memenuhi tuntutan tugas besar kedue strategi algoritma (IF2211) semester 4.
+Program ini digunakan untuk mencari lokasi sebuah file dalam kumplan folder di dalam folder menggunakan algoritma BFS dan DFS.
+
+## Requirements
+1. Windows Operating System (Tested on Windows 11)
+2. Windows Media Player (`WMP.dll`)
+3. Audio Ouput Device (For maximum experience)
+4. Repository ini
+
+## How to Run
+- Jalankan file `\bin\....exe`
+- Pilih Folder awal pencarian
+- Pilih algoritma pencari
+
+
+## Project Status
+Project is: _complete_
+
+## Sources
+- [Spesifikasi Tugas Besar 2](https://informatika.stei.itb.ac.id/~rinaldi.munir/Stmik/2021-2022/Tugas-Besar-2-IF2211-Strategi-Algoritma-2022.pdf)
+- [Panduan Penggunaan MSAGL untuk Visualisasi Graph oleh Asisten](https://docs.google.com/document/d/1XhFSpHU028Gaf7YxkmdbluLkQgVl3MY6gt1t-PL30LA/edit)
+- [Repository automatic-graph-layout by Microsoft](https://github.com/microsoft/automatic-graph-layout)
+
+## Acknowledgements
+- Thanks to God 
+- Thanks to Mrs. Masayu Leylia Khodra, Mrs. Nur Ulfa Maulidevi, and Mr. Rinaldi Munir as our lecturers
+- Thanks to academic assistants
+- Thanks to our lovely barudaks in the sky (colleagues)
+- This project was created to fulfill our Big Project for IF221 Strategi Algoritma
+
+## Contact
+Created by Bing Slamet Berdikari 2022 All Rights Reserved.
---
+++ src/WinFormsApp1/BFS.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+
+namespace WinFormsApp1
+{
+    public class BFS
+    {
+        private string startdir;
+        private string filename;
+        private List<dirTree> tree;
+        private Graph graph;
+
+        public BFS(string startdir, string filename)
+        {
+            this.startdir = @startdir;
+            this.filename = filename;
+            tree = new List<dirTree>();
+            graph = new Graph();
+        }
+        public List<dirTree> getTree()
+        {
+            return this.tree;
+        }
+        public Graph getGraph()
+        {
+            return this.graph;
+        }
+        
+
+        public void findFinal(string a)
+        {
+            string[] directories = this.startdir.Split("\\");
+            string final = "";
+            foreach(string x in directories)
+            {
+                final = x;
+            }
+            string y = this.filename;
+            //node root
+            Node test;
+            while (y != final)
+            {
+                test = this.graph.FindNode(y);
+
+                if(test != null)
+                {
+                    test.Attr.FillColor = Microsoft.Msagl.Drawing.Color.Green;
+                    foreach (Edge b in test.InEdges)
+                    {
+                        b.Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+                        test = b.SourceNode;
+                    }
+                    test.Attr.FillColor = Microsoft.Msagl.Drawing.Color.Green;
+                    y = test.Id;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void bfs_search(int set, ref List<string> lokasi)
+        {
+            var options = new EnumerationOptions()
+            {
+                IgnoreInaccessible = true, AttributesToSkip = FileAttributes.System
+            };
+            int i = 0;
+            bool found = false;
+            tree.Add(new dirTree(-999, Path.GetFileName(startdir), startdir,"Folder","Not"));
+            string parentdir = startdir;
+            while (i < tree.Count())
+            {
+                List<string> temptree = Directory.GetFiles(tree[i].directory,"*",options).ToList();
+                foreach (string a in temptree)
+                {
+                    if ((Path.GetFileName(a) == this.filename))
+                    {
+                        tree.Add(new dirTree(i, Path.GetFileName(a), a, "File", "Found"));
+                        found = true;
+                        lokasi.Add(a);
+                    }
+                    else
+                    {
+                        tree.Add(new dirTree(i, Path.GetFileName(a), a, "File", "Not"));
+                    }
+                    graph.AddEdge(tree[i].name, Path.GetFileName(a));
+                }
+                
+                if (set == 0 && found)
+                {
+                    return;
+                }
+                temptree = Directory.GetDirectories(tree[i].directory,"*",options).ToList(); ;
+                foreach (string a in temptree)
+                {
+                    tree.Add(new dirTree(i, Path.GetFileName(a), a, "Folder", "Not"));
+                    graph.AddEdge(tree[i].name, Path.GetFileName(a));
+                }
+                try
+                {
+                    i++;
+                    parentdir = tree[i].directory;
+                    while (tree[i].type == "File" && i < tree.Count())
+                    {
+                        i++;
+                        parentdir = tree[i].directory;
+                    }
+                }
+                catch { break; }
+            }
+        }
+    }
+}
